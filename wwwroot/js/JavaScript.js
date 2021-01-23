b6a2bac466be675571f71881c2f8a0ba07110d4a
yousefkhalil0311@gmail.com <yousefkhalil0311@gmail.com>
2021-01-23 09:32:22 -0500

    a

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,11 @@
+﻿var username = 'Yousef';
+var password = 'password';
+
+function verify(x, y) {
+
+    if (x == username && y == password) {
+        return 'Verification Successful';
+    } else {
+        return 'Failed Verification';
+    }
+}
